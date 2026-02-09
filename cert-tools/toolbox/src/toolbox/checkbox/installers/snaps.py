17ad73691ff9a71214f35d8f7d960d2caeebd600
Paolo Gentili <paolo.gentili@canonical.com>
2026-02-09 14:47:07 +0100

    fix: temporarily disable the new checkbox interface

--- cert-tools/toolbox/src/toolbox/checkbox/installers/snaps.py
+++ cert-tools/toolbox/src/toolbox/checkbox/installers/snaps.py
@@ -196,7 +196,7 @@ class CheckboxSnapsInstaller(CheckboxInstaller):
             policy=Linear(times=30, delay=10),
         )
 
-        if self.custom_frontend_interface():
+        if False and self.custom_frontend_interface():  # XXX currently broken
             logger.info("Using new providers interface with runtime agent")
             agent = self.runtime
         else:
